34dfd606e822d199df39c684a35d86de514aa0c7
Felix <none@none.none>
2018-11-02 18:00:43 +0100

    Re-enable tap2wake

--- ix5_repo_update.sh
+++ ix5_repo_update.sh
@@ -14,7 +14,9 @@ popd () {
     command popd "$@" > /dev/null
 }
 
-echo "+++ applying ***ix5*** patches +++"
+echo "+++                                               +++"
+echo "+++      applying ***ix5*** patches               +++"
+echo "+++                                               +++"
 
 
 pushd $ANDROOT/device/sony/tone
@@ -27,6 +29,17 @@ git fetch ix5
 git fetch $LINK af592265685fddf24100cbc1fdcdcb5bfd2260c1 && git cherry-pick FETCH_HEAD
 # Disable dm-verity
 git fetch $LINK b611c8d91a374f246be393d89f20bbf3fc2ab9f7 && git cherry-pick FETCH_HEAD
+popd
+
+
+pushd $ANDROOT/device/sony/kagura
+LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-kagura"
+(git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
+git fetch ix5
+
+# git checkout 'taptowake'
+# Re-enable tap to wake
+git fetch $LINK ffc06d3e7befb4e7234243481101bd5324b9609f && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/device/sony/common
@@ -49,6 +62,8 @@ popd
 
 # because "set -e" is used above, when we get to this point, we know
 # all patches were applied successfully.
-echo "+++ all ***ix5**** patches applied successfully! +++"
+echo "+++                                               +++"
+echo "+++ all *** ix5 *** patches applied successfully! +++"
+echo "+++                                               +++"
 
 set +e
