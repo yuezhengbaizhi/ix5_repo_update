4a58af25344db35681a87220b4e95c4e3cb8e01d
Felix <none@none.none>
2018-12-31 17:08:00 +0100

    adjust for 8.1

--- ix5_repo_update.sh
+++ ix5_repo_update.sh
@@ -36,22 +36,7 @@ echo "         applying ix5 patches...                  "
 echo ""
 
 
-pushd $ANDROOT/kernel/sony/msm-4.9/kernel
-# Enable wakeup_gesture in dtsi table
-# You need to discard vendor-sony-kernel or the build system will use
-# precompiled dtb files, thus rendering this patch useless
-git am < $PATCHES_PATH/kernel-dtsi-wakeup.patch
-# tone: panel: set min brightness to 1.2mA
-git am < $PATCHES_PATH/panel-minimum-brightness.patch
-#tone: Enable ToF sensor for kagura and keyaki
-git am < $PATCHES_PATH/kernel-enable-tof-sensor.patch
-#tone: Enable RGBCIR sensor
-git am < $PATCHES_PATH/kernel-rgbcr-sensor.patch
-popd
-
-# pushd $ANDROOT/kernel/sony/msm-4.9/kernel/arch/arm64/configs/sony
-# Tone: use DRM for display
-# git am < $PATCHES_PATH/defconfig-tone-use-drm.patch
+# pushd $ANDROOT/kernel/sony/msm-4.9/kernel
 # popd
 
 pushd $ANDROOT/packages/apps/Bluetooth
@@ -59,48 +44,10 @@ pushd $ANDROOT/packages/apps/Bluetooth
 git am < $PATCHES_PATH/bluetooth-disable-email-test.patch
 popd
 
-pushd $ANDROOT/device/sony/common
-LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-common"
-(git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
-git fetch ix5
-# git checkout 'selinux-enforcing'
-# Switch selinux to enforcing
-git fetch $LINK 1fc8e752c33ae07fe8c8f6d48abb2d1324b64536 && git cherry-pick FETCH_HEAD
-set +e
-if [ $(git tag -l "selinux-enforcing-temp-tag") ]; then
-    git tag -d selinux-enforcing-temp-tag
-fi
-set -e
-git tag selinux-enforcing-temp-tag
-# TEMP: Set PRODUCT_FULL_TREBLE_OVERRIDE false
-git fetch $LINK 0db7d41ae0d76f4d2a8627622921936d4e0626a8 && git cherry-pick FETCH_HEAD
-set +e
-if [ $(git tag -l "treble-override-temp-tag") ]; then
-    git tag -d treble-override-temp-tag
-fi
-set -e
-git tag treble-override-temp-tag
-
-# git checkout 'add-vendor-ix5'
-# Include vendor-ix5
-git fetch $LINK b115cc3f7f98c1d26a6bd8b84422706128e3d0b7 && git cherry-pick FETCH_HEAD
-# git checkout 'disable-privapp-perms-enforce'
-# Disable: enforce privapp permissions
-#git fetch $LINK 1418cfe786dbd046c13a0e2783af954056a45094 && git cherry-pick FETCH_HEAD
-
-# git checkout 'tether-ipac-fix'
-# marijns ipacm fix
-#git fetch $LINK f75d5f0b81e3c011c05c75db7e55ddc22c432f39 && git cherry-pick FETCH_HEAD
-
-# git checkout 'adreno-symlinks'
-# hardware: adreno: Move symlink creation to phony module.
-git fetch $LINK a69dcd7df5918017c936f5669562d1f9fedcb2c3 && git cherry-pick FETCH_HEAD
-# common: Depend on adreno_symlinks module.
-git fetch $LINK 10718b429c71cd32d6fea2b941e667b0bc9b3e18 && git cherry-pick FETCH_HEAD
-# common: Implement global symlink creation build-module style.
-git fetch $LINK 8c429ae594a353bc8929c982f3d8f00a3469eb7d && git cherry-pick FETCH_HEAD
-
-popd
+# pushd $ANDROOT/device/sony/common
+# LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-common"
+# (git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
+# popd
 
 pushd $ANDROOT/device/sony/tone
 LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-tone"
@@ -112,44 +59,21 @@ git fetch ix5
 git fetch $LINK af592265685fddf24100cbc1fdcdcb5bfd2260c1 && git cherry-pick FETCH_HEAD
 # Disable dm-verity
 git fetch $LINK b611c8d91a374f246be393d89f20bbf3fc2ab9f7 && git cherry-pick FETCH_HEAD
-
-# Revert "media_profiles: increase video recording framerate"
-#git revert a8cf2908fa80def497f9f312edd86402954627b8 --no-edit
 popd
 
 
-pushd $ANDROOT/device/sony/sepolicy
-
-LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-sepolicy"
-(git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
-git fetch ix5
-
-# TODO: Pick proper commits, branch is WIP right now
-#git checkout ix5/broad-updates
-
-# git checkout 'dt2w'
-# Add contexts for wakeup_gesture
-#git fetch $LINK 2cca619f81b7cfa9fbac9c209db1e752362f7ada && git cherry-pick FETCH_HEAD
-popd
+# pushd $ANDROOT/device/sony/sepolicy
+# LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-sepolicy"
+# (git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
+# git fetch ix5
+# popd
 
 
-pushd $ANDROOT/device/sony/kagura
-LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-kagura"
-(git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
-git fetch ix5
-
-# git checkout 'dt2w'
-# Re-enable tap to wake
-git fetch $LINK 90a80f6e42bfd2feca40fbdc8e2b046ff654032a && git cherry-pick FETCH_HEAD
-# Turn dt2w off by default in settings
-git fetch $LINK bc9df19ac1561281f2b10238d9007a803cfaaa06 && git cherry-pick FETCH_HEAD
-# git checkout 'brightness'
-# Set minimum brightness values to 2 and 1
-git fetch $LINK 449f9eccfd292d968a98d08546062aedbf6e1a2d && git cherry-pick FETCH_HEAD
-# git checkout 'rgbcir'
-# Add preliminary RGBCIR calibration file
-git fetch $LINK a0253f3de75c52bccb9275ee7eda6cd2f9db539c && git cherry-pick FETCH_HEAD
-popd
+# pushd $ANDROOT/device/sony/kagura
+# LINK=$HTTP && LINK+="://git.ix5.org/felix/device-sony-kagura"
+# (git remote --verbose | grep -q $LINK) || git remote add ix5 $LINK
+# git fetch ix5
+# popd
 
 # because "set -e" is used above, when we get to this point, we know
 # all patches were applied successfully.
