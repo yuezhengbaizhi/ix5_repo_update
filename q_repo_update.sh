3e38375ae3b9f520bf8b43d929d9724b568d5fec
ix5 <ix5@users.noreply.github.com>
2019-08-09 23:40:10 +0200

    Q: Disable matlog sepolicy exception for now

--- q_repo_update.sh
+++ q_repo_update.sh
@@ -278,9 +278,10 @@ apply_pull_commit 22 32827304b117684a3cd2a2ff3d8d115ffc0246f1
 apply_pull_commit 22 b3bdde9600dda7f41da63b2c55e14afd77fc5af8
 popd
 
-pushd $ANDROOT/system/sepolicy
-git am < $PATCHES_PATH/q-sepolicy-app-neverallow-exception-matlog.patch
-popd
+# Disabled for now
+#pushd $ANDROOT/system/sepolicy
+#git am < $PATCHES_PATH/q-sepolicy-app-neverallow-exception-matlog.patch
+#popd
 
 # because "set -e" is used above, when we get to this point, we know
 # all patches were applied successfully.
